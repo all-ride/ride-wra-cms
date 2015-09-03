4fd079b7555897944fbb0d46964a6c2c13befc77
Brent Roose <brent.roose@gmail.com>
2015-09-03 15:38:50 +0200

    Update site urlVariable calls

--- README.md
+++ README.md
@@ -4,7 +4,9 @@ This module provides API calls for the CMS layer.
 
 ## Available API calls
 
-- GET /api/v1/cms/sites/*%siteId%*/nodes/*%revision%*
+- GET /api/v1/cms/site/*%siteId%*/urlVariables/*%revision%*
     - Get all the nodes of a site with a revision
-- GET /api/v1/cms/sites/*%siteId%*/nodes
+- GET /api/v1/cms/site/*%siteId%*/urlVariables
     - Get all the nodes of a site
+- GET /api/v1/cms/sites/urlVariables
+    - Get all the nodes of all sites
--- src/ride/wra/cms/controller/SiteController.php
+++ src/ride/wra/cms/controller/SiteController.php
@@ -13,6 +13,11 @@ use ride\web\base\controller\AbstractController;
 class SiteController extends AbstractController {
 
     /**
+     * @var Cms $cms
+     */
+    private $cms;
+
+    /**
      * Get the nodes of a site and an optional revision. The response is parsed to work with Redactor
      *
      * @param Cms $cms
@@ -23,14 +28,61 @@ class SiteController extends AbstractController {
      *
      * @return Response
      */
-    public function siteNodesAction(Cms $cms, Request $request, I18n $i18n, $siteId, $revision=null) {
+    public function siteUrlVariablesAction(Cms $cms, Request $request, I18n $i18n, $siteId, $revision=null) {
+        $this->cms = $cms;
+
         // Get the default revision if not set
         if (!$revision) {
-            $revision = $cms->getDefaultRevision();
+            $revision = $this->cms->getDefaultRevision();
         }
 
+        // Get the nodes and set the response
+        $nodes = $this->getNodesForSite($siteId, $revision, $i18n->getLocale()->getCode());
+        $this->setJsonView($nodes);
+    }
+
+    /**
+     * Get the nodes of all sites. The response is parsed to work with Redactor
+     *
+     * @param Cms $cms
+     * @param Request $request
+     * @param I18n $i18n
+     * @param string $siteId
+     * @param string $revision
+     *
+     * @return Response
+     */
+    public function sitesUrlVariablesAction(Cms $cms, Request $request, I18n $i18n) {
+        $this->cms = $cms;
+        $revision = $this->cms->getDefaultRevision();
+        $sites = $this->cms->getSites();
+        $locale = $i18n->getLocale()->getCode();
+        $nodes = [];
+
+        foreach ($sites as $siteId=>$site) {
+            // Get the nodes of this site and add them to the result array
+            $siteNodes = $this->getNodesForSite($siteId, $revision, $locale);
+
+            foreach ($siteNodes as $node) {
+                $nodes[] = $node;
+            }
+        }
+
+        $this->setJsonView($nodes);
+    }
+
+    /**
+     * Parse the nodes of a site into an array
+     *
+     * @param string $siteId
+     * @param string $revision
+     * @param string $locale
+     *
+     * @return array
+     */
+    protected function getNodesForSite($siteId, $revision, $locale) {
         // Get all the nodes
-        $nodes = $cms->getNodeModel()->getNodes($siteId, $revision);
+        $nodes = $this->cms->getNodeModel()->getNodes($siteId, $revision);
         if (!count($nodes)) {
             // Set repsonse to 404 if no nodes were found
             return $this->response->setNotFound();
@@ -38,18 +90,18 @@ class SiteController extends AbstractController {
 
         // Get the nodelist from the rootnode
         $rootNode = reset($nodes)->getRootNode();
-        $nodeList = $cms->getNodeList($rootNode, $i18n->getLocale()->getCode());
+        $nodeList = $this->cms->getNodeList($rootNode, $locale, true, true, false);
         $result = [];
+
         // Add each node to the result array if the ID is not null
         foreach ($nodeList as $id=>$url) {
             if ($id) {
-                $result[] = [ 'name' => $url, 'url' => '[[node.'.$nodes[$id]->getId().'.url]]' ];
+                $nodeId = $nodes[$id]->getId();
+                $result[] = [ 'name' => $url, 'url' => "[[page.$siteId.$nodeId.$locale.url]]" ];
             }
         }
-        // Sort the nodes by url
-        sort($result);
 
-        $this->setJsonView($result);
+        return $result;
     }
 
 }
